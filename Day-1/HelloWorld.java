925f3bc7646c0ec5f450bfb3c2043ce3e8c16f80
Shannen Nazareno <nzrnshannen@gmail.com>
2023-02-18 19:16:54 +0800

    added day-1

---
+++ Day-1/HelloWorld.java
@@ -0,0 +1,20 @@
+
+/*
+
+Java application 
+
+ All the program is enclosed in a class definition: class HelloWorld
+ first method (function) that is run when the program is executed: main()
+
+ Java source files are named the same name as the class they define, with
+ the extension of .java
+
+ */
+
+class HelloWorld
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World!");
+    }
+}
